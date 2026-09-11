d9fb21e7d0059f1fee43c3c2d68561cb9ff89fbe
tanviknexora <tanvik@nexora.live>
2026-09-11 16:35:19 +0400

    Update brf_performance_app.py

--- brf_performance_app.py
+++ brf_performance_app.py
@@ -34,7 +34,7 @@ import streamlit as st
 # ----------------------------------------------------------------------
 st.set_page_config(
     page_title="BRF Referral Performance",
-    page_icon="📊",
+    page_icon="",
     layout="wide",
     initial_sidebar_state="expanded",
 )
